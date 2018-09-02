838d13f60b237da1cf7a56faa4a6f822266cabc4
prologic <cstha2@gmail.com>
2018-09-03 00:14:19 +0800

    first commit

---
+++ erebusengine_module/src/main/java/com/engine/erebus/erebusengine_module/Point.java
@@ -0,0 +1,19 @@
+package com.engine.erebus.erebusengine_module;
+
+public class Point {
+    float mLatitude;
+    float mLongitude;
+
+    public Point(float latitude, float longitude) {
+        mLatitude = latitude;
+        mLongitude = longitude;
+    }
+
+    public float getLatitude() {
+        return mLatitude;
+    }
+
+    public float getLongitude() {
+        return mLongitude;
+    }
+}
